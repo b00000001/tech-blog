9cbb0abca91954442290ba3073238b4fa2057c30
Aaron Deas <deas.aaron@gmail.com>
2021-07-03 04:09:21 -0400

    Finished building login system

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,34 +1,29 @@
 const router = require("express").Router();
-
-router.post("/", (req, res) => {
-  console.log(req.body);
-});
+const { User } = require("../../models");
 
 router.post("/login", async (req, res) => {
   try {
     const userData = await User.findOne({ where: { email: req.body.email } });
-
     if (!userData) {
       res
-      .status(400)
-      .json({ message: 'Incorrect email or password, please try again' })
+        .status(400)
+        .json({ message: "Incorrect email or password, please try again" });
       return;
     }
     const validPassword = await userData.checkPassword(req.body.password);
 
     if (!validPassword) {
       res
-      .status(400)
-      .json({ message: 'Incorrect email or password, please try again'})
+        .status(400)
+        .json({ message: "Incorrect email or password, please try again" });
       return;
     }
     req.session.save(() => {
-      req.session.user_id = userData.id;
+      req.session.user_id = userData.dataValues.id;
       req.session.logged_in = true;
-      res.json ({ user: userData, message: 'You are now logged in'})
-    })
-
-  } catch (err) { 
+      res.json({ user: userData.dataValues, message: "You are now logged in" });
+    });
+  } catch (err) {
     res.status(400).json(err);
   }
 });
--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 const path = require("path");
 const express = require("express");
 const routes = require("./controllers");
+const session = require("express-session");
 const exphbs = require("express-handlebars");
+
+const sequelize = require("./config/connection");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const hbs = exphbs.create({});
+
+const sess = {
+  secret: "Super secret secret",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({ db: sequelize })
+};
+
+app.use(session(sess));
 
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
@@ -17,4 +31,6 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use(routes);
 
-app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
+});
