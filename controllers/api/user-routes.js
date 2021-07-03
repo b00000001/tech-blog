feb1ad67e9ce79301cca4c8b426d679c0eaf971a
Aaron Deas <deas.aaron@gmail.com>
2021-07-02 20:11:31 -0400

    Added more to login system

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -4,7 +4,31 @@ router.post("/", (req, res) => {
   console.log(req.body);
 });
 
-router.post("/login", (req, res) => {
-  console.log(req.body);
+router.post("/login", async (req, res) => {
+  try {
+    const userData = await User.findOne({ where: { email: req.body.email } });
+
+    if (!userData) {
+      res
+      .status(400)
+      .json({ message: 'Incorrect email or password, please try again' })
+      return;
+    }
+    const validPassword = await userData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+      .status(400)
+      .json({ message: 'Incorrect email or password, please try again'})
+      return;
+    }
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+      res.json ({ user: userData, message: 'You are now logged in'})
+    })
+
+  }
 });
+
 module.exports = router;
