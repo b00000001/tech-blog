f7c265bed16c9e2286bd3678c257a8792f003576
Aaron Deas <deas.aaron@gmail.com>
2021-07-06 03:00:52 -0400

    Made it so post data shows on screen when logged in

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,39 +2,35 @@ const router = require("express").Router();
 const { User, Posts } = require("../models");
 
 router.get("/", async (req, res) => {
-  console.log(req.session);
-  // try {
-  //   const dbPostData = await Posts
-  //     .findAll
-  //     // {
-  //     //   attributes: ["id", "title"],
-  //     //   include: [{ model: User, attributes: ["username"] }]
-  //     // }
-  //     // {
-  //     //   model: User,
-  //     //   attributes: ["username"]
-  //     // }
-  //     ();
-  //   const posts = dbPostData.map((post) => post.get({ plain: true }));
-  //   res.render("login", { posts, loggedIn: req.session.loggedIn });
-  // } catch (err) {
-  //   res.status(500).json(err);
-  // }
   res.render("login");
 });
 
 router.get("/login", (req, res) => {
   if (req.session.loggedIn) {
-    res.redirect("/");
+    res.redirect("/homepage");
     return;
   }
 });
 
 router.get("/homepage", async (req, res) => {
-  const currentUserData = await User.findByPk(req.session.user_id);
-  const user = currentUserData.get({ plain: true });
-  res.render("homepage", {
-    user
-  });
+  console.log(req.session);
+  try {
+    const currentUserData = await User.findByPk(req.session.user_id);
+    const user = currentUserData.get({ plain: true });
+    const dbPostData = await Posts.findAll({
+      // attributes: ["id", "title"],
+      // include: [
+      //   {
+      //     model: User,
+      //     attributes: ["username"]
+      //   }
+      // ]
+    });
+    const posts = await dbPostData.map((post) => post.get({ plain: true }));
+    console.log("Post:", posts);
+    res.render("homepage", { posts, loggedIn: req.session.loggedIn });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 module.exports = router;
