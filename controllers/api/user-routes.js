ff91564d04c4aa9d71e03d6a05c5c3b32886f65d
Aaron Deas <deas.aaron@gmail.com>
2021-07-08 00:50:56 -0400

    Completed sign in logic so that new users can be added to the database

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,6 +1,17 @@
 const router = require("express").Router();
 const { User } = require("../../models");
 
+router.get("/", async (req, res) => {
+  try {
+    const dbUserData = await User.findAll({
+      attributes: { exclude: ["password"] }
+    });
+    res.json(dbUserData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 router.post("/", async (req, res) => {
   try {
     const dbUserData = await User.create({
@@ -12,9 +23,9 @@ router.post("/", async (req, res) => {
     req.session.save(() => {
       req.session.loggedIn = true;
       req.session.user_id = dbUserData.id;
-      return res.status(200).json(dbUserData);
+      req.session.username = dbUserData.username;
+      res.status(200).json(dbUserData);
     });
-    return;
   } catch (err) {
     console.log(err);
     return res.status(500).json(err);
