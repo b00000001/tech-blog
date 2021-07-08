426dc4982bbea058476a05ef99bab631859087d1
Aaron Deas <deas.aaron@gmail.com>
2021-07-08 16:33:07 -0400

    Realized comments were needed, starting building model for Comments

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -61,4 +61,22 @@ router.put("/:id", withAuth, async (req, res) => {
   }
 });
 
+router.delete("/:id", withAuth, async (req, res) => {
+  try {
+    const dbPostData = await Posts.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    if (!dbPostData) {
+      res.status(404).json({ message: "No post found with this id" });
+      return;
+    }
+    res.json(dbPostData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
