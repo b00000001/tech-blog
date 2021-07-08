ea3b63adb712b0a612ac47778ddd4eb73d8e39dc
Aaron Deas <deas.aaron@gmail.com>
2021-07-08 17:58:38 -0400

    Added home post route for /comments

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -12,4 +12,21 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.post("/", withAuth, async (req, res) => {
+  if (req.session) {
+    try {
+      const dbCommentData = await Comment.create({
+        comment_text: req.body.comment_text,
+        post_id: req.body.post_id,
+        // use the id from the session
+        user_id: req.session.user_id
+      });
+      return res.json(dbCommentData);
+    } catch (err) {
+      console.log(err);
+      res.status(400).json(err);
+    }
+  }
+});
+
 module.exports = router;
