d5263b4dd418982c17b58d1e1ce2dd3208b52630
Aaron Deas <deas.aaron@gmail.com>
2021-07-08 17:46:29 -0400

    Added comment-routes file for handling comment routes

---
+++ controllers/api/comment-routes.js
@@ -0,0 +1,13 @@
+const router = require("express").Router();
+const { Comment } = require("../../models");
+const withAuth = require("../../utils/auth");
+
+router.get("/", async (req, res) => {
+  try {
+    const dbCommentData = await Comment.findAll({});
+    return res.json(dbCommentData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
